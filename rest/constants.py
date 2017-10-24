d9df6ef364850d300192acd4aa2add2c474cb73c
maciejbiesek <maciejbiesek@gmail.com>
2017-10-24 17:39:40 +0200

    add constants file, refactoring

---
+++ rest/constants.py
@@ -0,0 +1,9 @@
+DATABASE_URL = "../database/database.db"
+NO_EXIST_MSG = "Page does not exist"
+ADDED_SUCCESS = "File %s added to the database"
+
+
+class JsonField:
+    Document = 'document'
+    Details = 'details'
+    Content = 'content'
